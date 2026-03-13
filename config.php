170379ace3418339c3ab25104065e2ef87a15bc0
Hayder Sabah <drhsg36@gmail.com>
2026-03-14 01:43:05 +0300

    update1

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 //$con = mysqli_connect("192.168.0.116", "haider", "1985", "clinic");
-$con = mysqli_connect("192.168.66.106", "haider", "1985", "clinic");
+//$con = mysqli_connect("192.168.66.106", "haider", "1985", "clinic");
 
 
-//$con = mysqli_connect("localhost", "root", "", "clinic");
+$con = mysqli_connect("localhost", "root", "", "clinic");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
