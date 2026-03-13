072de70d8d902f2942163da4d83dd77120f7de89
haydersabah85 <drhsg36@gmail.com>
2026-03-13 14:48:52 +0300

    Fix title spelling in main.php

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ include 'auth.php';
 
 <head>
     <meta charset="UTF-8">
-    <title>عيادة الدكتور حيدر صباح الربيعي</title>
+    <title>عيادة الدكتور حيدر صبا الربيعي</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script src="assets/theme.js" defer></script>
