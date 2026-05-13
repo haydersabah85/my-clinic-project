c79fe3e7a230fd0a22f16ac6bc1f5d251861036c
haydersabah85 <drhsg36@gmail.com>
2026-05-13 21:24:31 +0300

    deleted:    sync_to_online.php

--- sync_to_online.php
+++ sync_to_online.php
@@ -22,6 +22,11 @@ function syncTable($con, $url, $dataKey, $tableName) {
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);   // مهلة الاتصال
+curl_setopt($ch, CURLOPT_TIMEOUT, 120);         // المهلة الكلية
+
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         'Content-Type: application/json'
