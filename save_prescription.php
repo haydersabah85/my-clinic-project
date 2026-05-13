c669f8d34d6a1981030467c51955742ce8bb3bc7
haydersabah85 <drhsg36@gmail.com>
2026-05-13 21:48:51 +0300

    new_prescription.php

--- save_prescription.php
+++ save_prescription.php
@@ -11,18 +11,9 @@ $getPatient = mysqli_query($con, "
     FROM add_patient
     WHERE id = '$patient_id'
 ");
-
 $patientData = mysqli_fetch_assoc($getPatient);
 $patient_uuid = $patientData['uuid'];
-
-$getVisit = mysqli_query($con, "
-    SELECT visit_uuid
-    FROM patient_visits
-    WHERE id = '$visit_id'
-");
-
-$visitData = mysqli_fetch_assoc($getVisit);
-$visit_uuid = $visitData['visit_uuid'];
+$visit_uuid = bin2hex(random_bytes(16));
 
 
 if (empty($patient_id)) {
@@ -34,8 +25,8 @@ if (mysqli_num_rows($check) == 0) {
     die("خطأ: المريض غير موجود في قاعدة البيانات");
 }
 
-mysqli_query($con, "INSERT INTO prescriptions (patient_id, diagnosis, prescription_uuid, visit_uuid, patient_uuid, updated_at $syncFields) 
-VALUES ('$patient_id','$diagnosis','$prescription_uuid','$visit_uuid','$patient_uuid', NOW() $syncValues)");
+mysqli_query($con, "INSERT INTO prescriptions (patient_id, diagnosis, updated_at $syncFields) 
+VALUES ('$patient_id','$diagnosis', NOW() $syncValues)");
 
 $prescription_id = mysqli_insert_id($con);
 
@@ -49,30 +40,13 @@ foreach ($_POST['medicine_id'] as $index => $medicine_id) {
     $item_uuid = bin2hex(random_bytes(16));
     $syncFields = $IS_LOCAL ? ", sync_status" : "";
     $syncValues = $IS_LOCAL ? ", 0" : "";
-    $getPrescription = mysqli_query($con, "
-    SELECT prescription_uuid
-    FROM prescriptions
-    WHERE id = '$prescription_id'
-");
-
-    $prescriptionData = mysqli_fetch_assoc($getPrescription);
-    $prescription_uuid = $prescriptionData['prescription_uuid'];
-
-    $getMedicine = mysqli_query($con, "
-    SELECT medicine_uuid
-    FROM medicines
-    WHERE id = '$medicine_id'
-");
-
-    $medicineData = mysqli_fetch_assoc($getMedicine);
-    $medicine_uuid = $medicineData['medicine_uuid'];
-
+   
 
 
     mysqli_query($con, "INSERT INTO prescription_items 
-    (prescription_id, medicine_id, dose, frequency, duration, eye, instructions, item_uuid, prescription_uuid, medicine_uuid, updated_at $syncFields)
+    (prescription_id, medicine_id, dose, frequency, duration, eye, instructions, updated_at $syncFields)
     VALUES 
-    ('$prescription_id','$medicine_id','$dose','$frequency','$duration','$eye','$instructions','$item_uuid','$prescription_uuid','$medicine_uuid', NOW() $syncValues)");
+    ('$prescription_id','$medicine_id','$dose','$frequency','$duration','$eye','$instructions', NOW() $syncValues)");
 }
 
 header("Location: view_prescription.php?id=" . $prescription_id);
